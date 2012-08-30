7f521709d56656d42494955efcda67e9e8de6540
Matthias S. Benkmann <mux2005@gmail.com>
2012-08-30 12:53:21 +0200

    Parsing of config file /etc/gosa-si/server.conf

--- config/config.go
+++ config/config.go
@@ -1,8 +1,36 @@
+/* 
+Copyright (c) 2012 Landeshauptstadt München
+Author: Matthias S. Benkmann
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, 
+MA  02110-1301, USA.
+*/
+
 // Manages all user-configurable flags and variables.
 package config
 
-import "../util"
-import "crypto/aes"
+import (
+         "io"
+         "os"
+         "fmt"
+         "bufio"
+         "strings"
+         "crypto/aes"
+         
+         "../util"
+       )
 
 // The initialization vector for the AES encryption of GOsa messages.
 var InitializationVector = []byte(util.Md5sum("GONICUS GmbH")[0:aes.BlockSize])
@@ -20,6 +48,55 @@ var LogFilePath = "/var/log/go-susi.log"
 // Parses the relevant configuration files and sets the config variables
 // accordingly.       
 func ReadConfig() {
-  //TODO  
+  file, err := os.Open("/etc/gosa-si/server.conf")
+  if err != nil {
+    fmt.Fprintf(os.Stderr, "ERROR! ReadConfig: %v", err)
+    return
+  }
+  defer file.Close()
+  input := bufio.NewReader(file)
+  
+  conf := map[string]map[string]string{"":map[string]string{}}
+  current_section := ""
+  for {
+    var line string
+    line, err = input.ReadString('\n')
+    if err != nil { break }
+    
+    line = strings.TrimSpace(line)
+    if len(line) > 2 && line[0] == '[' && line[len(line)-1] == ']' {
+      current_section = line
+      if _, ok := conf[current_section]; !ok {
+        conf[current_section] = map[string]string{}
+      }
+    }
+    
+    i := strings.Index(line, "=")
+    if i >= 0 {
+      key := strings.TrimSpace(line[0:i])
+      value := strings.TrimSpace(line[i+1:])
+      if key != "" {
+        conf[current_section][key] = value
+      }
+    }
+  }
+  
+  if err != io.EOF {
+    fmt.Fprintf(os.Stderr, "ERROR! ReadString: %v", err)
+    // Do not return. Try working with whatever we got out of the file.
+  }
+  
+  for _, section := range conf {
+    if sectkey, ok := section["key"]; ok {
+      ModuleKeys = append(ModuleKeys, sectkey)
+    }
+  }
+  
+  if general, ok := conf["[general]"]; ok {
+    if logfile, ok := general["log-file"]; ok {
+      LogFilePath = logfile
+    }
+  }
+  
 }
 
