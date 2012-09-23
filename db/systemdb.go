ad43f817b71675b3de12e024b1721c3ce05ea666
Matthias S. Benkmann <matthias.benkmann@gmail.com>
2012-09-23 17:13:35 +0200

    implemented job_trigger_action_*

---
+++ db/systemdb.go
@@ -0,0 +1,31 @@
+/*
+Copyright (c) 2012 Matthias S. Benkmann
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, 
+MA  02110-1301, USA.
+*/
+
+// API for the various databases used by go-susi.
+package db
+
+// Returns the common name for the system with the given MAC address.
+// The name may or may not include a domain. In fact technically the name
+// may be anything.
+// Returns "unknown" if the name could not be determined. Since this is a valid
+// system name, you should NOT special case this (e.g. use it to check if
+// the system is known).
+func SystemNameForMAC(macaddress string) string {
+  return "unknown"
+}  
---
+++ message/job_trigger_action.go
@@ -0,0 +1,63 @@
+/* 
+Copyright (c) 2012 Matthias S. Benkmann
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, 
+MA  02110-1301, USA.
+*/
+
+package message
+
+import (
+         "regexp"
+         "strings"
+         "encoding/base64"
+         
+         "../db"
+         "../xml"
+         "../config"
+       )
+
+var macAddressRegexp = regexp.MustCompile("^[:xdigit:](:[:xdigit:]){5}$")
+
+// Handles all messages of the form "job_trigger_action_*".
+//  xmlmsg: the decrypted and parsed message
+// Returns:
+//  unencrypted reply
+func job_trigger_action(xmlmsg *xml.Hash) string {
+  job := xml.NewHash("job")
+  job.Add("progress", "none")
+  job.Add("status", "waiting")
+  job.Add("siserver", config.ServerSourceAddress)
+  job.Add("modified", "1")
+  job.Add("targettag", xmlmsg.Text("target"))
+  macaddress := xmlmsg.Text("macaddress")
+  if macaddress == "" { macaddress = xmlmsg.Text("target") }
+  macaddress = strings.ToLower(macaddress)
+  if !macAddressRegexp.MatchString(macaddress) {
+    return ErrorReply("job_trigger_action* with invalid or missing MAC address")
+  }
+  job.Add("macaddress", macaddress)
+  plainname := db.SystemNameForMAC(macaddress)
+  job.Add("plainname", plainname)
+  timestamp := xmlmsg.Text("timestamp")
+  if timestamp == "" { timestamp = "19700101000000" }
+  job.Add("timestamp", timestamp)
+  job.Add("headertag", xmlmsg.Text("header")[len("job_"):])
+  job.Add("result", "none")
+  job.Add("xmlmessage", base64.StdEncoding.EncodeToString([]byte(xmlmsg.String())))
+  return ""
+}
+
+
