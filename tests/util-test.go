30a9bff93f1a90f01da08ffef244d113a1bf7a41
Matthias S. Benkmann <matthias.benkmann@gmail.com>
2012-11-25 21:40:36 +0100

    fix typo in package description

--- tests/util-test.go
+++ tests/util-test.go
@@ -17,7 +17,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 MA  02110-1301, USA.
 */
 
-// Unit tests run by run_tests.go.
+// Unit tests run by run-tests.go.
 package tests
 
 import (
