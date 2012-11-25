89b2d93845c22c5289746071097921bc9630b665
Matthias S. Benkmann <matthias.benkmann@gmail.com>
2012-11-25 21:42:40 +0100

    new functions db.SystemGetState() and db.SystemSetState() for getting/setting gotoMode and faiState

--- db/systemdb.go
+++ db/systemdb.go
@@ -22,8 +22,10 @@ package db
 
 import (
          "fmt"
+         "bytes"
          "strings"
          "os/exec"
+         "encoding/base64"
          
          "../xml"
          "../util"
@@ -51,11 +53,58 @@ func PlainnameForMAC(macaddress string) string {
   return strings.SplitN(name, ".", 2)[0]
 }  
 
+// Replaces the attribute attrname with the sole value attrvalue for the system
+// identified by the given macaddress.
+//
+// ATTENTION! This function accesses LDAP and may therefore take a while.
+// If possible you should use it asynchronously.
+func SystemSetState(macaddress string, attrname, attrvalue string) {
+  system, err := xml.LdifToHash("", true, LdapSearch(fmt.Sprintf("(&(objectClass=GOHard)(macAddress=%v)%v)",macaddress, config.UnitTagFilter),"dn"))
+  dn := system.Text("dn")
+  if dn == "" {
+    util.Log(0, "ERROR! Could not get dn for MAC %v: %v", macaddress, err)
+    return
+  }
+  out, err := LdapModify(dn, attrname, attrvalue).CombinedOutput()
+  if err != nil {
+    util.Log(0, "ERROR! Could not change state of object %v: %v (%v)",dn,err,string(out))
+  }
+}
+
+// Returns the 1st value of attribute attrname for the system
+// identified by the given macaddress.
+//
+// ATTENTION! This function accesses LDAP and may therefore take a while.
+// If possible you should use it asynchronously.
+func SystemGetState(macaddress string, attrname string) string {
+  system, err := xml.LdifToHash("", true, LdapSearch(fmt.Sprintf("(&(objectClass=GOHard)(macAddress=%v)%v)",macaddress, config.UnitTagFilter),attrname))
+  if err != nil {
+    util.Log(0, "ERROR! Could not get attribute %v for MAC %v: %v", attrname, macaddress, err)
+    return ""
+  }
+  return system.Text(strings.ToLower(attrname))
+}
+
 func LdapSearch(query string, attr... string) *exec.Cmd {
   args := []string{"-x", "-LLL", "-H", config.LDAPURI, "-b", config.LDAPBase}
   if config.LDAPUser != "" { args = append(args,"-D",config.LDAPUser,"-w",config.LDAPUserPassword) }
   args = append(args, query)
   args = append(args, attr...)
-  util.Log(1, "INFO! ldapsearch %v",args)
+  util.Log(2, "DEBUG! ldapsearch %v",args)
   return exec.Command("ldapsearch", args...)
+}
+
+func LdapModify(dn string, attrname, attrvalue string) *exec.Cmd {
+  args := []string{"-x", "-H", config.LDAPURI}
+  args = append(args,"-D",config.LDAPAdmin,"-w",config.LDAPAdminPassword)
+  util.Log(2, "DEBUG! ldapmodify %v (Set %v to '%v' for %v)",args, attrname, attrvalue, dn)
+  cmd := exec.Command("ldapmodify", args...)
+  cmd.Stdin = bytes.NewBufferString(fmt.Sprintf(`dn:: %v
+changetype: modify
+replace: %v
+%v:: %v
+`,base64.StdEncoding.EncodeToString([]byte(dn)),
+  attrname,
+  attrname,base64.StdEncoding.EncodeToString([]byte(attrvalue))))
+  return cmd
 }
