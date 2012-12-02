57a2c679346171a5f95c41b49d8796df469be288
Matthias S. Benkmann <matthias.benkmann@gmail.com>
2012-12-02 16:02:09 +0100

    make "force localboot" remove pending install and update jobs

--- action/localboot.go
+++ action/localboot.go
@@ -1,8 +1,31 @@
 package action
 
-import "../db"
-import "../xml"
+import (
+         "time"
+         
+         "../db"
+         "../xml"
+         "../util"
+       )
 
 func Localboot(job *xml.Hash) {
+  db.SystemSetState(job.Text("macaddress"), "faiState", "localboot")
+  
+  // remove softupdate and install jobs ...
+  job_types_to_kill := xml.FilterOr(
+                       []xml.HashFilter{xml.FilterSimple("headertag","trigger_action_install"),
+                                        xml.FilterSimple("headertag","trigger_action_update")})
+  // ... that are already happening or scheduled within the next 5s ...
+  timeframe := xml.FilterRel("timestamp", util.MakeTimestamp(time.Now().Add(5*time.Second)),-1,0)
+  // ... that affect the machine for which we force localboot
+  target := xml.FilterSimple("macaddress", job.Text("macaddress"))
+  
+  db.JobsRemove(xml.FilterAnd([]xml.HashFilter{ job_types_to_kill,
+                                                timeframe,
+                                                target }))
+  
+  // Wait a little and set state to localboot again, just in case the job
+  // removal raced with something that set the faiState
+  time.Sleep(2*time.Second)
   db.SystemSetState(job.Text("macaddress"), "faiState", "localboot")
 }
