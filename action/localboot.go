abd7f7435ab346840982ba171156a18125977685
Matthias S. Benkmann <matthias.benkmann@gmail.com>
2012-12-02 16:11:13 +0100

    make localboot cancel all install/update jobs in the next 5 MINUTES not just 5 seconds

--- action/localboot.go
+++ action/localboot.go
@@ -15,8 +15,8 @@ func Localboot(job *xml.Hash) {
   job_types_to_kill := xml.FilterOr(
                        []xml.HashFilter{xml.FilterSimple("headertag","trigger_action_install"),
                                         xml.FilterSimple("headertag","trigger_action_update")})
-  // ... that are already happening or scheduled within the next 5s ...
-  timeframe := xml.FilterRel("timestamp", util.MakeTimestamp(time.Now().Add(5*time.Second)),-1,0)
+  // ... that are already happening or scheduled within the next 5 minutes ...
+  timeframe := xml.FilterRel("timestamp", util.MakeTimestamp(time.Now().Add(5*time.Minute)),-1,0)
   // ... that affect the machine for which we force localboot
   target := xml.FilterSimple("macaddress", job.Text("macaddress"))
   
