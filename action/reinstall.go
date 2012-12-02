f267e9a1e9f88ffa592b7b5384ecc1cfc0de405d
Matthias S. Benkmann <matthias.benkmann@gmail.com>
2012-12-02 22:30:12 +0100

    basic implementation of reinstall and update

--- action/reinstall.go
+++ action/reinstall.go
@@ -1,6 +1,9 @@
 package action
 
+import "../db"
 import "../xml"
 
 func Reinstall(job *xml.Hash) {
+  db.SystemSetState(job.Text("macaddress"), "faiState", "install")
+  Reboot(job)
 }
--- action/update.go
+++ action/update.go
@@ -1,6 +1,9 @@
 package action
 
+import "../db"
 import "../xml"
 
 func Update(job *xml.Hash) {
+  db.SystemSetState(job.Text("macaddress"), "faiState", "softupdate")
+  Reboot(job)
 }
