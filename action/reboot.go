1073f638558ef75a7e9ca2ee8b4400f199682a57
Matthias S. Benkmann <matthias.benkmann@gmail.com>
2012-12-02 22:35:44 +0100

    imcomplete implementation of reboot

--- action/reboot.go
+++ action/reboot.go
@@ -3,4 +3,11 @@ package action
 import "../xml"
 
 func Reboot(job *xml.Hash) {
+  /*
+  send reboot command to gosa-si-client
+  */
+  
+  // In case the machine is sleeping, wake it up, because user
+  // expects machine to be on after a reboot.
+  Wake(job)
 }
