d7bbaa8907d8aec2a96f660994b80bfd104c9cbc
Matthias S. Benkmann <matthias.benkmann@gmail.com>
2012-12-02 20:56:00 +0100

    Make "force localboot" more forceful

--- action/localboot.go
+++ action/localboot.go
@@ -9,23 +9,36 @@ import (
        )
 
 func Localboot(job *xml.Hash) {
-  db.SystemSetState(job.Text("macaddress"), "faiState", "localboot")
   
-  // remove softupdate and install jobs ...
-  job_types_to_kill := xml.FilterOr(
-                       []xml.HashFilter{xml.FilterSimple("headertag","trigger_action_install"),
-                                        xml.FilterSimple("headertag","trigger_action_update")})
-  // ... that are already happening or scheduled within the next 5 minutes ...
-  timeframe := xml.FilterRel("timestamp", util.MakeTimestamp(time.Now().Add(5*time.Minute)),-1,0)
-  // ... that affect the machine for which we force localboot
-  target := xml.FilterSimple("macaddress", job.Text("macaddress"))
+  // retry for 30s
+  endtime := time.Now().Add(30*time.Second)
   
-  db.JobsRemove(xml.FilterAnd([]xml.HashFilter{ job_types_to_kill,
-                                                timeframe,
-                                                target }))
+  for ; time.Now().Before(endtime);  {
+    db.SystemSetState(job.Text("macaddress"), "faiState", "localboot")
+    
+    // remove softupdate and install jobs ...
+    job_types_to_kill := xml.FilterOr(
+                         []xml.HashFilter{xml.FilterSimple("headertag","trigger_action_reinstall"),
+                                          xml.FilterSimple("headertag","trigger_action_update")})
+    // ... that are already happening or scheduled within the next 5 minutes ...
+    timeframe := xml.FilterRel("timestamp", util.MakeTimestamp(time.Now().Add(5*time.Minute)),-1,0)
+    // ... that affect the machine for which we force localboot
+    target := xml.FilterSimple("macaddress", job.Text("macaddress"))
+    filter := xml.FilterAnd([]xml.HashFilter{ job_types_to_kill,
+                                                  timeframe,
+                                                  target })
+    db.JobsRemove(filter)
+    
+    // Wait a little and see if the jobs are gone
+    time.Sleep(3*time.Second)
+    if len(db.JobsQuery(filter).Subtags()) == 0 { 
+      // set state again just in case the job removal raced with something that set faistate
+      db.SystemSetState(job.Text("macaddress"), "faiState", "localboot")
+      return 
+    }
+    
+    util.Log(2, "DEBUG! Force localboot: Some install/softupdate jobs remain => Retrying")
+  }
   
-  // Wait a little and set state to localboot again, just in case the job
-  // removal raced with something that set the faiState
-  time.Sleep(2*time.Second)
-  db.SystemSetState(job.Text("macaddress"), "faiState", "localboot")
+  util.Log(0, "ERROR! Can't remove install/softupdate jobs to force localboot.")
 }
