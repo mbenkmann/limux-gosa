375e7585c54841fd3e7038dc31afb1c8f67065c4
Matthias S. Benkmann <mux2005@gmail.com>
2013-03-04 13:56:29 +0100

    Rewrite of client_connection.go to use a new TCP connection for every message This simplies the code and makes it more robust. The drawback is that the order of messages is no longer preserved. But I don't see a problem with this ATM.

--- message/client_connection.go
+++ message/client_connection.go
@@ -27,7 +27,6 @@ import (
           
          "../db"
          "../util"
-         "../util/deque"
          "../config"
        )
 
@@ -54,44 +53,112 @@ type ClientMessage struct {
   Expires time.Time
 }
 
-// A connection to a client, permanent if the client is locally registered.
+// A connection to a client. 
 // A ClientConnection is obtained via the Client() function. 
 // All communication with clients that go-susi initiates is
 // performed via ClientConnections.
+//
+// NOTE:
+// Up to revision 5a9da13080de this
+// was a permanent connection similar to peer_connection.go.
+// However while examining issue #58 I came to the conclusion
+// that there's no real need to maintain client connections
+// as permanent connections, because unlike
+// server-server-communication there's no need to preserve
+// message order when sending messages to clients (at least
+// at this time I'm not aware of such a need).
+//
 type ClientConnection struct {
-  // FIFO input queue of ClientMessage elements.
-  // The Tell() function enqueues messages here.
-  queue deque.Deque
-  
-  // ClientMessage elements from the input queue are moved into this buffer 
-  // for processing. They are removed from this buffer only after they have
-  // either been sent successfully or have expired.
-  // NOTE: The separation between queue and buffer is done for technical
-  // reasons because it makes the code of handleConnection() more elegant
-  // because it avoids using explicit channels or timers and instead works
-  // solely with Deque's methods.
-  buffer deque.Deque
-  
   // IP:PORT of the peer.
   addr string
-  
-  // If the client is locally registered, this TCP connection is used to contact
-  // it. The connection is kept open and will be used until an error occurs or
-  // we discover the client is no longer registered here.
-  tcpConn net.Conn
 }
 
-// Tell(msg, ttl): Tries to send msg to the client. If the client is locally
-//                 registered the message will be sent directly, otherwise it
+// Tell(msg, ttl): Tries to send text to the client.
+//                 If the client is locally
+//                 registered the message will be sent directly (encrypted with
+//                 the client's key), otherwise it
 //                 will be forwarded via the server responsible for the client.
 //                 The ttl determines how long the message will be buffered for
 //                 resend attempts if sending fails. ttl==0 (or some other
 //                 small ttl) should be set for
 //                 messages that are only of interest to locally registered
 //                 clients (like "registered", or "new_ldap_config")
-func (conn *ClientConnection) Tell(msg string, ttl time.Duration) {
-  util.Log(2, "DEBUG! Queuing message for client %v with TTL %v: %v", conn.addr, ttl, msg)
-  conn.queue.Push(ClientMessage{msg, time.Now().Add(ttl)})
+func (conn *ClientConnection) Tell(text string, ttl time.Duration) {
+  util.Log(2, "DEBUG! Tell(): Queuing message for client %v with TTL %v: %v", conn.addr, ttl, text)
+
+  msg := &ClientMessage{text, time.Now().Add(ttl)}
+  
+  go util.WithPanicHandler(func(){
+    var try uint = 0
+  
+    for {
+      if try > 0 {
+        expiry := msg.Expires.Sub(time.Now())
+        if expiry <= 0 { 
+          break
+        }
+        delay := (1 << try) * time.Second
+        if delay > 60*time.Second { delay = 60*time.Second }
+        if delay > expiry { delay = expiry }
+        util.Log(2, "DEBUG! Sleeping %v before next send attempt", delay)
+        time.Sleep(delay)
+      }
+    
+      try++
+      
+      util.Log(1, "INFO! Attempt #%v to send message to %v: %v", try, conn.addr, msg.Text)
+      
+      client := db.ClientWithAddress(conn.addr)
+      if client == nil {
+        util.Log(0, "ERROR! Client %v not found in clientdb")
+        continue
+      }
+      
+      // if client is registered at a foreign server
+      if client.Text("source") != config.ServerSourceAddress {
+        util.Log(1, "INFO! Client %v is registered at %v => Forwarding message", conn.addr, client.Text("source"))
+        
+        // MESSAGE FORWARDING NOT YET IMPLEMENTED
+        util.Log(0, "ERROR! Message forwarding not yet implemented")
+        break
+        
+      } else { // if client is registered at our server
+      
+        keys := client.Get("key")
+        if len(keys) == 0 {
+          // This case should be impossible. A client's here_i_am message always contains a key (unless the client is buggy).
+          util.Log(0, "ERROR! No key known for client %v", conn.addr)
+          break
+        }
+      
+        encrypted := GosaEncrypt(msg.Text, keys[0])
+      
+        tcpConn, err := net.Dial("tcp", conn.addr)
+        if err != nil {
+          util.Log(0,"ERROR! Dial() could not connect to %v: %v",conn.addr, err)
+          continue
+        }
+        
+        err = tcpConn.(*net.TCPConn).SetKeepAlive(true)
+        if err != nil {
+          util.Log(0, "ERROR! SetKeepAlive: %v", err)
+          // This is not fatal => Don't abort send attempt
+        }
+        
+        util.Log(2, "DEBUG! Sending message to %v encrypted with key %v", conn.addr, keys[0])
+        err = util.SendLn(tcpConn, encrypted, config.Timeout) 
+        tcpConn.Close()
+        if err == nil { 
+          util.Log(2, "DEBUG! Successfully sent message to %v: %v", conn.addr, msg.Text)
+          return // not break! break would cause an error message to be logged
+        } else {
+          util.Log(0, "ERROR! SendLn() to %v failed: %v", conn.addr, err)
+        }
+      }
+    }  
+    
+    util.Log(0, "ERROR! Cannot send message to %v: %v", conn.addr, msg.Text)
+  })
 }
 
 // Maps IP:ADDR to a ClientConnection object that talks to that peer. All accesses
@@ -134,162 +201,9 @@ func Client(addr string) *ClientConnection {
   if !have_already {
     conn = &ClientConnection{addr:addr}
     client_connections[addr] = conn
-    go util.WithPanicHandler(func(){conn.handleConnection()})
   }
   return conn
 }
 
-// Tries to actually send message to the client. Returns true if success, 
-// false if failure.
-func (conn *ClientConnection) tryToSend(message ClientMessage) bool {
-  util.Log(2, "DEBUG! Trying to send message to client %v: %v", conn.addr, message.Text)
-  
-  var err error
-  
-  client := db.ClientWithAddress(conn.addr)
-  if client == nil {
-    util.Log(0, "ERROR! Client %v not found in clientdb", conn.addr)
-    if conn.tcpConn != nil { conn.tcpConn.Close() }
-    conn.tcpConn = nil
-    return false
-  }
-  
-  // if client is registered at a foreign server
-  if client.Text("source") != config.ServerSourceAddress {
-    if conn.tcpConn != nil { conn.tcpConn.Close() }
-    conn.tcpConn = nil
-    
-    util.Log(1, "INFO! Client %v is registered at %v => Forwarding message", conn.addr, client.Text("source"))
-    
-    // MESSAGE FORWARDING NOT YET IMPLEMENTED
-    util.Log(0, "ERROR! Message forwarding not yet implemented")
-    
-  } else { // if client is registered at our server
-  
-    keys := client.Get("key")
-    if len(keys) == 0 {
-      util.Log(0, "ERROR! ClientConnection.tryToSend: No key known for client %v", conn.addr)
-      return false
-    }
-  
-    encrypted := GosaEncrypt(message.Text, keys[0])
-  
-    if conn.tcpConn != nil { // try sending via existing connection if it exists
-      util.Log(2, "DEBUG! ClientConnection.tryToSend() to %v encrypted with key %v via existing connection", conn.addr, keys[0])
-      err = util.SendLn(conn.tcpConn, encrypted, config.Timeout) 
-      if err == nil { 
-        util.Log(2, "DEBUG! ClientConnection.tryToSend() successfully sent message to %v: %v", conn.addr, message.Text)
-        return true 
-      }
-        
-      conn.tcpConn.Close()
-      conn.tcpConn = nil
-    }
-    
-    // try to (re-)establish connection
-    util.Log(2, "DEBUG! ClientConnection.tryToSend() attempting to (re-)establish connection to %v", conn.addr)
-    conn.tcpConn, err = net.Dial("tcp", conn.addr)
-    if err != nil {
-      util.Log(0,"ERROR! ClientConnection.tryToSend() failed to establish connection to %v: %v",conn.addr,err)
-      return false
-    }
-    
-    err = conn.tcpConn.(*net.TCPConn).SetKeepAlive(true)
-    if err != nil {
-      util.Log(0, "ERROR! SetKeepAlive: %v", err)
-      // This is not fatal => Don't abort
-    }
-    
-    // Fix for issue #58
-    // use monitorConnection from peer_connection.go because otherwise we can lose messages because
-    // if the remote end is closed for whatever reason write(2) does not return an error immediately.
-    // Probably has to do with buffering. monitorConnection() however notices the EOF.
-    go monitorConnection(conn.tcpConn, deque.New())
-    
-    // try to send message over newly established connection
-    util.Log(2, "DEBUG! ClientConnection.tryToSend() to %v encrypted with key %v via newly established connection", conn.addr, keys[0])
-    err = util.SendLn(conn.tcpConn, encrypted, config.Timeout) 
-    if err == nil { 
-      util.Log(2, "DEBUG! ClientConnection.tryToSend() successfully sent message to %v: %v", conn.addr, message.Text)
-      return true 
-    }
-    
-    conn.tcpConn.Close()
-    conn.tcpConn = nil
-  }
-  
-  return false
-}
-
-// Runs in its own goroutine and attempts to actually transmit the messages
-// stored in conn's buffers. Message expiry is also managed here.
-func (conn *ClientConnection) handleConnection() {
-  var delay time.Duration
-  
-  // REMINDER:
-  //   conn.queue: input queue. Tell() pushes messages into this queue.
-  //   conn.buffer: output buffer. Messages to be actually sent are taken from this
-  //                buffer. Messages move conn.queue => conn.buffer => TCP connection
-
-  for {
-    // if no messages buffered, reset resend delay to infinity
-    if conn.buffer.IsEmpty() { delay = 0 }
-    
-    // wait for either new input or expiry of delay for resend attempt
-    conn.queue.WaitForItem(delay)
-    
-    // append new input to buffer
-    for m := conn.queue.RemoveAt(0); m != nil; m = conn.queue.RemoveAt(0) {
-      conn.buffer.Push(m)
-    }
-    
-    // try to send messages
-    for ; !conn.buffer.IsEmpty(); {
-      
-      // gosa-si puts incoming messages into incomingdb and then
-      // processes them in the order they are returned by the database
-      // which causes messages to be processed in the wrong order.
-      // To counteract this we wait a little between messages.
-      // The wait time may seem long, but even with as much as 250ms
-      // I observed the fju for a new job and the fju that adds the
-      // plainname getting mixed up. Apparently gosa-si takes time
-      // in the seconds range to process messages.
-      // If we have >= 10 messages backlog, we don't wait. 
-      //
-      // NOTE: The delay is intentionally placed BEFORE the sending attempt.
-      // This guarantees that there will be a delay between all messages.
-      // If the delay was placed anywhere else it's easy (especially as a
-      // side effect of well-intentioned optimizations) to get situations
-      // where the delay may be skipped.
-      if conn.buffer.Count()+conn.queue.Count() < 10 { time.Sleep(1000*time.Millisecond) }
-      
-      message := conn.buffer.Next().(ClientMessage)
-      
-      if !conn.tryToSend(message) { // sending failed
-        // put message back into buffer
-        util.Log(2, "DEBUG! Failed to send message to %v. Queuing for resend: ", conn.addr, message.Text)
-        conn.buffer.Insert(message)
-        
-        // increase delay until resend
-        delay = (delay + 1*time.Second) * 2
-        if delay > 60*time.Second { delay = 60*time.Second }
-        
-        // remove expired messages from buffer
-        now := time.Now()
-        for i := 0; i < conn.buffer.Count(); {
-          if conn.buffer.Peek(i).(ClientMessage).Expires.Before(now) {
-            message = conn.buffer.RemoveAt(i).(ClientMessage)
-            util.Log(0, "ERROR! Discarding expired message to %v: %v", conn.addr, message.Text)
-          } else {
-            i++
-          }
-        }
-        
-        // back to main loop
-        break
-      }
-    }
-  }
-}
 
 
