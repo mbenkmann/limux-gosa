6cdc2693bac1d333a3adfff4a61b6c5767a3ee94
Matthias S. Benkmann <mux2005@gmail.com>
2013-03-12 18:29:40 +0100

    [feature] Implemented action.Reboot(). If we're lucky all jobs magically start working to some degree.

--- action/reboot.go
+++ action/reboot.go
@@ -1,11 +1,24 @@
 package action
 
-import "../xml"
+import (
+         "../db"
+         "../xml"
+         "../util"
+         "../config"
+         "../message"
+       )
 
 func Reboot(job *xml.Hash) bool {
-  /*
-  send reboot command to gosa-si-client
-  */
+  client := db.ClientWithMAC(job.Text("macaddress"))
+  if client == nil {
+    util.Log(0, "ERROR! Client with MAC %v unknown. Cannot execute job: %v", job.Text("macaddress"), job)
+    return false
+  } 
+  
+  client_addr := client.Text("client")
+  util.Log(1, "INFO! Sending trigger_action_reboot to %v", client_addr)
+  trigger_action_reboot := "<xml><header>trigger_action_reboot</header><trigger_action_reboot></trigger_action_reboot><source>"+config.ServerSourceAddress+"</source><target>"+client_addr+"</target></xml>"
+  message.Client(client_addr).Tell(trigger_action_reboot, config.LocalClientMessageTTL)
   
   // In case the machine is sleeping, wake it up, because user
   // expects machine to be on after a reboot.
