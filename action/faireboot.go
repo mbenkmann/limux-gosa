d35fc93525c60dab3d715bcf326198a140940b31
Matthias S. Benkmann <matthias.benkmann@gmail.com>
2013-04-20 15:54:56 +0200

    Implemented Issue #113: Support aborting of jobs

--- action/faireboot.go
+++ action/faireboot.go
@@ -1,6 +1,30 @@
 package action
 
-import "../xml"
+import (
+         "strings"
+         
+         "../db"
+         "../xml"
+         "../util"
+       )
 
 func FAIReboot(job *xml.Hash) {
+  macaddress := job.Text("macaddress")
+  
+  delete_system := false
+  faistate := "error:pxe:-1:crit:Job aborted. System in undefined state."
+  sys, err := db.SystemGetAllDataForMAC(macaddress, false)
+  if err != nil {
+    util.Log(0, "ERROR! FAIReboot(): %v", err)
+    // do not abort. Killing jobs may still work.
+  } else {
+    // If the system is in incoming, delete it because faimond-ldap does not
+    // cope well with incomplete LDAP objects and tries to boot them from local disk.
+    dnparts := strings.Split(sys.Text("dn"),",")
+    if len(dnparts) > 1 && dnparts[1] == "ou=incoming" { delete_system = true }
+  }
+  
+  ForceFAIState(macaddress, faistate)
+  
+  if delete_system { db.SystemReplace(sys, nil) }
 }
--- action/localboot.go
+++ action/localboot.go
@@ -9,12 +9,15 @@ import (
        )
 
 func Localboot(job *xml.Hash) {
-  
+  ForceFAIState(job.Text("macaddress"), "localboot")
+}
+
+func ForceFAIState(macaddress, faistate string) {
   // retry for 30s
   endtime := time.Now().Add(30*time.Second)
   
   for ; time.Now().Before(endtime);  {
-    db.SystemSetState(job.Text("macaddress"), "faiState", "localboot")
+    db.SystemSetState(macaddress, "faiState", faistate)
     
     // remove softupdate and install jobs ...
     job_types_to_kill := xml.FilterOr(
@@ -22,8 +25,8 @@ func Localboot(job *xml.Hash) {
                                           xml.FilterSimple("headertag","trigger_action_update")})
     // ... that are already happening or scheduled within the next 5 minutes ...
     timeframe := xml.FilterRel("timestamp", util.MakeTimestamp(time.Now().Add(5*time.Minute)),-1,0)
-    // ... that affect the machine for which we force localboot
-    target := xml.FilterSimple("macaddress", job.Text("macaddress"))
+    // ... that affect the machine for which we force the faistate
+    target := xml.FilterSimple("macaddress", macaddress)
     filter := xml.FilterAnd([]xml.HashFilter{ job_types_to_kill,
                                                   timeframe,
                                                   target })
@@ -33,12 +36,12 @@ func Localboot(job *xml.Hash) {
     time.Sleep(3*time.Second)
     if db.JobsQuery(filter).FirstChild() == nil { // if all jobs are gone
       // set state again just in case the job removal raced with something that set faistate
-      db.SystemSetState(job.Text("macaddress"), "faiState", "localboot")
+      db.SystemSetState(macaddress, "faiState", faistate)
       return // we're done
     } // else if some jobs remained
     
-    util.Log(2, "DEBUG! Force localboot: Some install/softupdate jobs remain => Retrying")
+    util.Log(2, "DEBUG! ForceFAIState(%v, %v): Some install/softupdate jobs remain => Retrying", macaddress, faistate)
   }
   
-  util.Log(0, "ERROR! Can't remove install/softupdate jobs to force localboot.")
+  util.Log(0, "ERROR! ForceFAIState(%v, %v): Can't remove install/softupdate jobs.", macaddress, faistate)
 }
