60fd8b46e91dd2b48df70cdcb116f43025955083
Matthias S. Benkmann <mux2005@gmail.com>
2013-08-26 17:41:36 +0200

    added main/trigger_action script with the actual implementation of trigger_action_foo

--- message/trigger_action_foo.go
+++ message/trigger_action_foo.go
@@ -30,7 +30,6 @@ import (
 //  xmlmsg: the decrypted and parsed message
 func trigger_action_foo(xmlmsg *xml.Hash) {
   start := time.Now()
-  util.Log(1, "INFO! Running trigger-action-hook %v", config.TriggerActionHookPath)
   env := config.HookEnvironment()
   for _, tag := range xmlmsg.Subtags() {
     env = append(env, tag+"="+strings.Join(xmlmsg.Get(tag),"\n"))
@@ -38,6 +37,7 @@ func trigger_action_foo(xmlmsg *xml.Hash) {
   cmd := exec.Command(config.TriggerActionHookPath)
   env = append(env, "xml="+xmlmsg.String())
   cmd.Env = append(env, os.Environ()...)
+  util.Log(1, "INFO! Running trigger-action-hook %v with parameters %v", config.TriggerActionHookPath, env)
   out, err := cmd.CombinedOutput()
   if err != nil {
     util.Log(0, "ERROR! trigger-action-hook %v: %v (%v)", config.TriggerActionHookPath, err, string(out))
